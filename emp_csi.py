f3829d94686a8fe9ec8a20bb80607b0cdd3e6592
menyakina <menyakinaz@mail.ru>
2022-11-30 23:49:10 +0300

    добавили  функцию insert и show_row

--- emp_csi.py
+++ emp_csi.py
@@ -10,3 +10,24 @@ def file_open(file_name):
         for row in reader:
             csv_file.append(row)
         print("Файл открыт. Записей: ", len(csv_file))
+
+
+def insert(fio, age, tel, otd):
+    try:
+        mx = max(csv_file, key=lambda x: int(x['ном']))
+        csv_file.append({'ном': int(mx['ном']) + 1,
+                         'фио': fio,
+                         'возраст': age,
+                         'телефон': tel,
+                         'отдел': otd})
+    except Exception as e:
+        return f"Ошибка при добавлении новой записи {e}"
+    print("Данные добавлены")
+
+
+def show_rows():
+    if len(csv_file) > 0:
+        print("{:<5}{:<25}{:<8}{:<12}{:<15}" .format("ном", "фио", "возраст", "телефон", "отдел"))
+        for el in csv_file:
+            print("{:<5}{:<25}{:<8}{:<12}{:<15}" .format(el['ном'], el['фио'],
+                                                         el['возраст'], el['телефон'], el['отдел']))
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from emp_csi import file_open
+from emp_csi import file_open, insert, show_rows
 
 FILENAME = "data.csv"
 
@@ -20,5 +20,9 @@ while True:
     action = input(">_")
     if action == "1":
         file_open(FILENAME)
+    elif action == "2":
+        insert(input('фио: '), input('возраст: '), input('телефон: '), input('отдел: '))
+    elif action == "7":
+        show_rows()
     elif action == "exit":
         break
